cde8ce756ac0be73d4506746da8df66e6d685960
Lu, Hilbert <hilbert.lu@sap.com>
2017-03-24 15:38:36 +0800

    Initial Commit

---
+++ ext/daimlerspm/daimlerspminitialdata/src/com/daimler/spm/initialdata/constants/DaimlerspmInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.daimler.spm.initialdata.constants;
+
+/**
+ * Global class for all DaimlerspmInitialData constants.
+ */
+public final class DaimlerspmInitialDataConstants extends GeneratedDaimlerspmInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "daimlerspminitialdata";
+
+	private DaimlerspmInitialDataConstants()
+	{
+		//empty
+	}
+}
