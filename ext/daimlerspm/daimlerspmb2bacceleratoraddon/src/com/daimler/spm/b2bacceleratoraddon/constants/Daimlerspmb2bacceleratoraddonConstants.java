a32f1d8baa907dfc5d93710e512eb99846f5be3b
Lu, Hilbert <hilbert.lu@sap.com>
2017-03-31 15:45:50 +0800

    Use daimlerspmb2bacceleratoraddon to replace b2bcheckoutaddon from OOTB

---
+++ ext/daimlerspm/daimlerspmb2bacceleratoraddon/src/com/daimler/spm/b2bacceleratoraddon/constants/Daimlerspmb2bacceleratoraddonConstants.java
@@ -0,0 +1,28 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.daimler.spm.b2bacceleratoraddon.constants;
+
+/**
+ * Global class for all Daimlerspmb2bacceleratoraddon constants. You can add global constants for your extension into
+ * this class.
+ */
+public final class Daimlerspmb2bacceleratoraddonConstants extends GeneratedDaimlerspmb2bacceleratoraddonConstants
+{
+	public static final String EXTENSIONNAME = "daimlerspmb2bacceleratoraddon";
+	public static final String ADDON_PREFIX = "addon:/daimlerspmb2bacceleratoraddon";
+
+	private Daimlerspmb2bacceleratoraddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
