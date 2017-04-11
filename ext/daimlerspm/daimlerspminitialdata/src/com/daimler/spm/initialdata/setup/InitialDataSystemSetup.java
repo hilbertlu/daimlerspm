31ecf5e5704ec3e480106e5038359fa413212e11
Lu, Hilbert <hilbert.lu@sap.com>
2017-04-11 15:41:19 +0800

    Master Data Preparation: BaseStore2WarehouseRel

--- ext/daimlerspm/daimlerspminitialdata/src/com/daimler/spm/initialdata/setup/InitialDataSystemSetup.java
+++ ext/daimlerspm/daimlerspminitialdata/src/com/daimler/spm/initialdata/setup/InitialDataSystemSetup.java
@@ -135,22 +135,27 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/cms-content_de.impex", extensionName), false);
 		getSetupImpexService().importImpexFile(String.format(
 				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/cms-content_zh.impex", extensionName), false);
+		
 		getSetupImpexService().importImpexFile(String.format(
-				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/categories.impex", extensionName), false);
+				"/%s/import/sampledata/productCatalogs/powertoolsContentCatalog/categories.impex", extensionName), false);
 		getSetupImpexService().importImpexFile(String.format(
-				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/categories_en.impex", extensionName), false);
+				"/%s/import/sampledata/productCatalogs/powertoolsContentCatalog/categories_en.impex", extensionName), false);
 		getSetupImpexService().importImpexFile(String.format(
-				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/categories_zh.impex", extensionName), false);
+				"/%s/import/sampledata/productCatalogs/powertoolsContentCatalog/categories_zh.impex", extensionName), false);
 		getSetupImpexService().importImpexFile(String.format(
-				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/products.impex", extensionName), false);
+				"/%s/import/sampledata/productCatalogs/powertoolsContentCatalog/products.impex", extensionName), false);
 		getSetupImpexService().importImpexFile(String.format(
-				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/products_en.impex", extensionName), false);
+				"/%s/import/sampledata/productCatalogs/powertoolsContentCatalog/products_en.impex", extensionName), false);
 		getSetupImpexService().importImpexFile(String.format(
-				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/products_zh.impex", extensionName), false);
+				"/%s/import/sampledata/productCatalogs/powertoolsContentCatalog/products_zh.impex", extensionName), false);
 		getSetupImpexService().importImpexFile(String.format(
-				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/products-prices.impex", extensionName), false);	
+				"/%s/import/sampledata/productCatalogs/powertoolsContentCatalog/products-prices.impex", extensionName), false);	
 		getSetupImpexService().importImpexFile(String.format(
-				"/%s/import/sampledata/contentCatalogs/powertoolsContentCatalog/products-stocklevels.impex", extensionName), false);
+				"/%s/import/sampledata/productCatalogs/powertoolsContentCatalog/products-stocklevels.impex", extensionName), false);
+		
+		getSetupImpexService().importImpexFile(String.format(
+				"/%s/import/sampledata/stores/powertools/warehouses.impex", extensionName), false);
+
 		
 		getEventService().publishEvent(new SampleDataImportedEvent(context, importData));
 	}
